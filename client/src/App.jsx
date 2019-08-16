c2157388791720232fe0045c40c243cdfb825dd1
Yauhen <q010@bk.ru>
2019-08-16 11:51:47 +0300

    created configured application

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+
+function App() {
+  return (
+      <div>
+          hi
+      </div>
+  );
+}
+
+export default hot(App);
---
+++ client/src/constants.js
@@ -0,0 +1,12 @@
+export const API_HOST = 'localhost';
+
+export const API_PORT = '3001';
+
+export const API_ROOT = 'http://localhost:3001';
+
+export const API_WS_ROOT = 'ws://localhost:3001/cable';
+
+export const HEADERS = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+};
