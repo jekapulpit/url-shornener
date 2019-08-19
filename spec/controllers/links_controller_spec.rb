957a7a26ccec172ab899d6fa92db63062ccd3d73
Yauhen <q010@bk.ru>
2019-08-19 13:18:46 +0300

    added specs for links#redirect

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe LinksController, type: :controller do
-
+  describe 'GET redirect' do
+    let(:short_link_instance) { ShortLinkService.new('http://www.betterspecs.org').call }
+    let(:short_link) { short_link_instance.link_hash }
+    it 'redirects to right link' do
+      get :redirect, :params => { :link_hash => short_link }
+      expect(response.status).to eq(302)
+      response.should redirect_to short_link_instance.original_link
+    end
+  end
 end
