6df75ade848d43381f4f81ede6e9f9f41be707dc
Yauhen <q010@bk.ru>
2019-08-20 12:23:20 +0300

    removed reduntant files

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,10 +1,1 @@
-export const API_HOST = 'localhost';
-
-export const API_PORT = '3001';
-
 export const API_ROOT = 'http://localhost:3001';
-
-export const HEADERS = {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-};
